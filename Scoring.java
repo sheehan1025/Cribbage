daead46cc8ece91b9724a60e80077ad337ffd567
sheehan1025 <118309783+sheehan1025@users.noreply.github.com>
2023-09-27 09:47:11 -0700

    Remove boolean in runs replace with pairs check

--- Scoring.java
+++ Scoring.java
@@ -138,25 +138,17 @@ public class Scoring{
         //Todo consideration of a quad run
         int runCount = 0;
         int currRunCount = 1;
-        boolean isDoubleRun = false;
-        boolean isTripleRun = false;
         int pairsN = pairs(a);
         for(int i = 1; i < a.size(); i++){
             if(a.get(i).getValue() == a.get(i - 1).getValue() + 1){
                 currRunCount++;
             }
-            else if(a.get(i).getValue() == a.get(i - 1).getValue() && isDoubleRun){
-                isTripleRun = true;
-            }
-            else if(a.get(i).getValue() == a.get(i - 1).getValue() ){
-                isDoubleRun = true;
-            }
             else{
                 if (currRunCount >= 3) {
-                    if (isDoubleRun && !isTripleRun) {
+                    if (pairsN == 2) {
                         runCount += currRunCount * 2;
                     }
-                    else if (isTripleRun && pairsN == 6){
+                    else if (pairsN == 6){
                         runCount += currRunCount * 3;
                     }
                     else if(pairsN == 4){
@@ -167,15 +159,13 @@ public class Scoring{
                     }
                 }
                 currRunCount = 1;
-                isDoubleRun = false;
-                isTripleRun = false;
             }
         }
         if (currRunCount >= 3) {
-            if (isDoubleRun && !isTripleRun) {
+            if (pairsN == 2) {
                 runCount += currRunCount * 2;
             }
-            else if (isTripleRun && pairsN == 6) {
+            else if (pairsN == 6) {
                 runCount += currRunCount * 3;
             }
             else if(pairsN == 4){
